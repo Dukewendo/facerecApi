33ee46cc8ab685b0df80ddbe61804f239d499f43
Dukewendo <dommct@yahoo.co.uk>
2021-06-15 13:40:10 +0100

    server update

--- server.js
+++ server.js
@@ -14,14 +14,6 @@ app.use(bodyParser.json());
 //check if this is needed if not- delete it
 app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
-
 const db = knex({
   client: 'pg',
   connection: {
